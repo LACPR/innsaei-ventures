e70cb21b0c1c3bb21073a47a68bcda48dbc6d0c4
Chandima Ranasinghe <chandimapr@gmail.com>
2020-09-29 02:03:57 +0530

    Updates

---
+++ precache-manifest.c9203ac18097b2e6c8ff4de07e85480a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21ba650d53c1d576a2d488851c6f567",
+    "url": "/innsaei-ventures/index.html"
+  },
+  {
+    "revision": "123ed511b9b93514e848",
+    "url": "/innsaei-ventures/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "95e90ce9e84518898b3c",
+    "url": "/innsaei-ventures/static/css/main.81f5facc.chunk.css"
+  },
+  {
+    "revision": "123ed511b9b93514e848",
+    "url": "/innsaei-ventures/static/js/2.743e0a7f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/innsaei-ventures/static/js/2.743e0a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e90ce9e84518898b3c",
+    "url": "/innsaei-ventures/static/js/main.61389275.chunk.js"
+  },
+  {
+    "revision": "f42d5dc7c52ff1409782",
+    "url": "/innsaei-ventures/static/js/runtime-main.9425142e.js"
+  }
+]);
